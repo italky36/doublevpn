166640e7f7c84184950bbb0c2a22885e6eb17b20
italky36 <75229207+italky36@users.noreply.github.com>
2021-03-17 13:09:50 +0300

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -26,33 +26,34 @@ echo "Please wait..."
 ansible-playbook main.yml
 
 CNF=$(cat  /root/doublevpn/wg-client.conf);
+cp /root/doublevpn/wg-client.conf /etc/wireguard/wg-client.conf
 MYIP=$(curl -4 https://icanhazip.com/);
 
 #docker
-curl -fsSL https://get.docker.com/ | sh
-sudo systemctl start docker
-sudo systemctl enable docker
+#curl -fsSL https://get.docker.com/ | sh
+#sudo systemctl start docker
+#sudo systemctl enable docker
 
-sudo mkdir -p /var/lib/openvpn/mongodb
-sudo docker rm -f openvpn &> /dev/null
-sudo docker run \
-    --name openvpn \
-    --privileged \
-    --detach \
-    --privileged \
-    --restart=always \
-    --net=host \
-    -v /var/lib/openvpn/mongodb:/var/lib/mongodb\
-    -v /var/lib/openvpn:/var/lib/pritunl \
-      jippi/pritunl
+#sudo mkdir -p /var/lib/openvpn/mongodb
+#sudo docker rm -f openvpn &> /dev/null
+#sudo docker run \
+#    --name openvpn \
+#    --privileged \
+#    --detach \
+#    --privileged \
+#    --restart=always \
+#    --net=host \
+#    -v /var/lib/openvpn/mongodb:/var/lib/mongodb\
+#    -v /var/lib/openvpn:/var/lib/pritunl \
+#      jippi/pritunl
 
-echo " If you want to use wireguard, copy this text"
+echo " Конфиг ищи в файле /etc/wireguard/wg-client.conf или скопируй ниже текст"
 echo "#####################################################################################################################"
 echo "$CNF"
 echo "#####################################################################################################################"
-echo "If you want to use OpenVPN"
-echo "open in web browser    :  https://$MYIP"
-echo "Enter login/password   :  pritunl/pritunl"
+#echo "If you want to use OpenVPN"
+#echo "open in web browser    :  https://$MYIP"
+#echo "Enter login/password   :  pritunl/pritunl"
 
 rm -R /root/doublevpn &> /dev/null
 rm -R /root/1.sh &> /dev/null
